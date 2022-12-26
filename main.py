80403376ebff5a1dcbf5fe49f851a3ae1169231d
Soilenyis <soilenyis@gmail.com>
2022-12-26 10:58:18 -0300

    create rota de busca de curso por id

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ def create(request: CursoRequest, db: Session = Depends(get_db)):
 def find_all(db: Session = Depends(get_db)):
     cursos = CursoRepository.find_all(db)
     return [CursoResponse.from_orm(curso) for curso in cursos]
+
+
+@app.get("/api/cursos/{id}", response_model=CursoResponse)
+def find_by_id(id: int, db: Session = Depends(get_db)):
+    curso = CursoRepository.find_by_id(db, id)
+    if not curso:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado"
+        )
+    return CursoResponse.from_orm(curso)
+
